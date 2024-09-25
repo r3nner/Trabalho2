b713a05dd158e4493653b58d214a096c86492656
r3nner <163298782+r3nner@users.noreply.github.com>
2024-09-25 15:22:14 -0300

    Update pesquisa.h

--- pesquisa.h
+++ pesquisa.h
@@ -2,11 +2,11 @@
 
 #define pesquisa_h
 
-int direcao(int x, int y, char *palavra, char **matriz, int tamMatriz);
+int direcao(int x, int y, char *palavra, char **matriz, int l, int c);
 
-int restantePalavra(int x, int y, char palavra[], char **matriz, int tamMatriz);
+int restantePalavra(int x, int y, char palavra[], char **matriz, int l, int c);
 
 //faz a pesquisa
-void pesquisa(char **matriz, char palavra[], int tamMatriz);
+void pesquisa(char **matriz, char palavra[], int l, int c);
 
 #endif
